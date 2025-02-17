f8a9b46affd03aee1750015ca45cb8ae4f10edf3
Ray <rainer.musch@gmx.de>
2025-02-17 19:03:13 +0100

    update app html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { MainModule } from './main/main.module';
+import { AboutMeComponent } from './main/about-me/about-me.component';
+import { HeroComponent } from './main/hero/hero.component';
+import { MySkillsComponent } from './main/my-skills/my-skills.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { SayHiComponent } from './main/say-hi/say-hi.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule],
+  imports: [RouterOutlet, SharedModule, MainModule, AboutMeComponent, HeroComponent, MySkillsComponent, PortfolioComponent, SayHiComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 
 
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: []
 })
 export class MainModule { }
