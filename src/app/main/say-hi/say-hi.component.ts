17fa647032fc7eff06a0e8f7552400e23caca00d
Ray <rainer.musch@gmx.de>
2025-02-19 06:22:03 +0100

    add basic structure say hi  section

--- src/app/main/say-hi/say-hi.component.ts
+++ src/app/main/say-hi/say-hi.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-say-hi',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './say-hi.component.html',
   styleUrl: './say-hi.component.scss'
 })
 export class SayHiComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  privacyAccepted: boolean = false;
+  nameError: boolean = false;
+  emailError: boolean = false;
+  messageError: boolean = false;
 
+  validateName() {
+    this.nameError = !this.name.trim();
+  }
+
+  validateEmail() {
+    this.emailError = !this.email.trim();
+  }
+
+  validateMessage() {
+    this.messageError = !this.message.trim();
+  }
+
+  isFormValid(): boolean {
+    return !!this.name.trim() && !!this.email.trim() && !!this.message.trim() && this.privacyAccepted;
+  }
+  
+
+  sendMessage() {
+    if (this.isFormValid()) {
+      // später übergabe an mail einbauen
+      console.log('Form submitted', {
+        name: this.name,
+        email: this.email,
+        message: this.message
+      });
+    }
+  }
 }
