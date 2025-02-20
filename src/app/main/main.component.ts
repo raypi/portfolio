8ece950aba20f9000c40ffb552740c78a8a1b1bf
Ray <rainer.musch@gmx.de>
2025-02-20 18:47:13 +0100

    add content and responive to references

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,12 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SayHiComponent } from './say-hi/say-hi.component';
+import { ReferencesComponent } from './references/references.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, SayHiComponent],
+  imports: [HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, SayHiComponent, ReferencesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
--- src/app/main/references/references.component.ts
+++ src/app/main/references/references.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+
+interface Reference {
+  name: string;
+  title: string;
+  reference: string;
+}
+
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+
+  references: Reference[] = [
+    { name: 'V.Schuster', title: 'Team Partner', reference: 'Ray ist ein geiler Typ' },
+    { name: 'J. Müller', title: 'Direktor', reference: 'Ray wer ist eigentlich Ray' },
+    { name: 'T.Webelein', title: 'Team Partner', reference: 'Ray hat zwar eine rauhe Schale, aber einen weichen Kern' }
+  ];
 
 }
