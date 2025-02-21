b91bb6e30edb96e77cb23aeeaa06e604a9c32a80
Ray <rainer.musch@gmx.de>
2025-02-21 16:06:51 +0100

    uodate @fot loop to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,21 +1,25 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   
-    languages = [
+    languages: {
+      language: string;
+      img: string;
+      link: string;
+      active: boolean;
+    } [] = [
       { language: 'English', img: 'assets/img/flackeGb.png', link: '#', active: true },
       { language: 'German',  img: 'assets/img/flackeGermany.png', link: '#', active: false },
       { language: 'Spanish', img: 'assets/img/flackeSpain.png', link: '#', active: false },
       { language: 'Russian', img: 'assets/img/flackeRussia.png', link: '#', active: false }
-    ];
+    ]
 
     onLanguageSelect(selectedLang: any, event: Event) {
       event.preventDefault(); // Verhindert das Standardverhalten des Links
